5b3de0a7913a088de743272cdda6d685a5c0d902
Jeremy <jeremy.teurterie@gmail.com>
2023-03-28 17:12:46 +0700

    :building_construction: feat: add router architecture

---
+++ src/Router.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Error404 from './pages/Error404';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} errorElement={<Error404 />}></Route>
+      <Route path="*" element={<Error404 />} />
+    </>
+  )
+);
+
+function Router() {
+  return <RouterProvider router={router} />;
+}
+
+export default Router;
---
+++ src/pages/Error404.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../styles/Error404.module.css';
+
+const Error404 = () => {
+  return (
+    <section id={styles.error404}>
+      <h1>Erreur 404</h1>
+      <Link to="/">Retourner sur la page d'accueil</Link>
+    </section>
+  );
+};
+
+export default Error404;

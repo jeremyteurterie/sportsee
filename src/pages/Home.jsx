abeda1e0f0dd34840667290e95bc7fc492f2fbaa
Jeremy <jeremy.teurterie@gmail.com>
2023-03-31 11:08:30 +0700

    :construction: feat(wip): button redirection to profil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import styles from '../styles/Home.module.css';
 
 const Home = () => {
   const [id, setId] = useState();
+  const navigate = useNavigate();
+  const handleSubmit = () => {
+    navigate('/profil/');
+  };
   return (
     <>
       <Header />
@@ -12,6 +17,10 @@ const Home = () => {
         <button onClick={() => setId(18)} className={styles.buttonLayout}>
           CECILIA
         </button>
+        <button onClick={handleSubmit} className={styles.buttonLayout}>
+          TEST
+        </button>
+
         <button onClick={() => setId(12)} className={styles.buttonLayout}>
           KARL
         </button>

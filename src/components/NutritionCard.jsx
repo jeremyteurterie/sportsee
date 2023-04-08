9b8eca021b4ba4d7c49ec629328e988274e1c7c4
Jeremy <jeremy.teurterie@gmail.com>
2023-04-08 12:12:52 +0700

    :recycle: chore: refactor NutritionCard.jsx

--- src/components/NutritionCard.jsx
+++ src/components/NutritionCard.jsx
@@ -1,97 +1,68 @@
 import PropTypes from 'prop-types';
-
 import caloriesIcon from '../assets/energy.svg';
 import lipidesIcon from '../assets/cheeseburger.svg';
 import proteinesIcon from '../assets/chicken.svg';
 import glucidesIcon from '../assets/apple.svg';
-
 import styles from '../styles/NutritionCard.module.css';
 
-/**
- * A component that generates nutrition cards.
- *
- * @param {Object} props - The component props.
- * @param {Object} props.data - The nutrition data.
- * @returns {JSX.Element[]} An array of React elements that display the nutrition cards.
- */
 const NutritionCard = ({ data }) => {
   return generateCards(data);
 };
 
-/**
- * Generates nutrition cards from the given data.
- *
- * @param {Object} data - The nutrition data.
- * @returns {JSX.Element[]} An array of React elements that display the nutrition cards.
- */
 function generateCards(data) {
   const nutritionCardList = [];
   for (const [key, value] of Object.entries(data)) {
     nutritionCardList.push(
       <figure className={styles.cards} key={key}>
-        {mapKeyValue(key, value)}
+        {generateNutritionInfo(key, value)}
       </figure>
     );
   }
   return nutritionCardList;
 }
 
-/**
- * Maps a nutrition data key-value pair to a React element.
- *
- * @param {string} key - The nutrition data key.
- * @param {number} value - The nutrition data value.
- * @returns {JSX.Element} A React element that displays the nutrition data.
- */
-function mapKeyValue(key, value) {
-  switch (key) {
-    case 'calorieCount':
-      return (
-        <>
-          <img alt="" src={caloriesIcon} />
-          <figcaption>
-            <b>{value}KCal</b>
-            <br />
-            Calories
-          </figcaption>
-        </>
-      );
-    case 'proteinCount':
-      return (
-        <>
-          <img alt="" src={proteinesIcon} />
-          <figcaption>
-            <b>{value}g</b>
-            <br />
-            Proteines
-          </figcaption>
-        </>
-      );
-    case 'carbohydrateCount':
-      return (
-        <>
-          <img alt="" src={glucidesIcon} />
-          <figcaption>
-            <b>{value}g</b>
-            <br />
-            Glucides
-          </figcaption>
-        </>
-      );
-    case 'lipidCount':
-      return (
-        <>
-          <img alt="" src={lipidesIcon} />
-          <figcaption>
-            <b>{value}g</b>
-            <br />
-            Lipides
-          </figcaption>
-        </>
-      );
-    default:
-      break;
+function generateNutritionInfo(key, value) {
+  const nutritionInfo = {
+    calorieCount: {
+      imgSrc: caloriesIcon,
+      label: 'KCal',
+      name: 'Calories',
+    },
+    proteinCount: {
+      imgSrc: proteinesIcon,
+      label: 'g',
+      name: 'Proteines',
+    },
+    carbohydrateCount: {
+      imgSrc: glucidesIcon,
+      label: 'g',
+      name: 'Glucides',
+    },
+    lipidCount: {
+      imgSrc: lipidesIcon,
+      label: 'g',
+      name: 'Lipides',
+    },
+  };
+
+  if (key in nutritionInfo) {
+    const { imgSrc, label, name } = nutritionInfo[key];
+    return (
+      <>
+        <img alt="" src={imgSrc} />
+        <figcaption>
+          <b>
+            {value}
+            {label}
+          </b>
+          <br />
+          {name}
+        </figcaption>
+      </>
+    );
   }
+
+  return null;
 }
 
 NutritionCard.propTypes = {

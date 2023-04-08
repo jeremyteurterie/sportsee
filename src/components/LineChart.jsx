6f36d43b23ac936beca463c83d42000d88020cd1
Jeremy <jeremy.teurterie@gmail.com>
2023-04-08 12:06:40 +0700

    :recycle: chore: refactor linechart.jsx

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -11,47 +11,17 @@ import {
 import PropTypes from 'prop-types';
 import styles from '../styles/LineChart.module.css';
 
-/**
- * A component that renders a line chart showing the average session duration for each day of the week.
- * @param {object} props - The props object.
- * @param {array} props.durationData - An array of objects containing data for each day of the week, with each object containing a "day" property (number from 1-7) and a "sessionLength" property (number).
- * @returns {JSX.Element} - The rendered line chart component.
- */
-const DurationChart = ({ durationData }) => {
-  /**
-   * Converts a day number from 1-7 to a corresponding day letter abbreviation.
-   * @param {number} day - The day number (1-7).
-   * @returns {string} - The corresponding day letter abbreviation (e.g. "L" for Monday in French).
-   */
-  function convertDayNumberToDayLetter(day) {
-    switch (day) {
-      case 1:
-        return 'L';
-      case 2:
-        return 'M';
-      case 3:
-        return 'M';
-      case 4:
-        return 'J';
-      case 5:
-        return 'V';
-      case 6:
-        return 'S';
-      case 7:
-        return 'D';
-      default:
-        return '';
+const SessionsDurationChart = ({ durationData }) => {
+  function dayNumberToLetter(day) {
+    const dayLetters = ['L', 'M', 'M', 'J', 'V', 'S', 'D'];
+    if (day < 1 || day > 7) {
+      return '';
     }
+    return dayLetters[day - 1];
   }
 
-  /**
-   * A custom tooltip component that shows the session duration for the hovered data point.
-   * @param {object} props - The props object.
-   * @param {array} props.payload - An array of objects containing the data for the hovered data point.
-   * @param {boolean} props.active - Whether the tooltip is currently active (hovered over).
-   * @returns {JSX.Element} - The rendered tooltip component.
-   */
-  const TooltipGenerator = ({ payload, active }) => {
+  const DurationToolTip = (props) => {
+    const { payload, active } = props;
     if (active) {
       return (
         <div className={styles.durationtooltip}>
@@ -61,10 +31,6 @@ const DurationChart = ({ durationData }) => {
     }
   };
 
-  /**
-   * A component that renders the chart title.
-   * @returns {JSX.Element} - The rendered chart title component.
-   */
   const DurationChartTitle = () => {
     return (
       <>
@@ -101,14 +67,14 @@ const DurationChart = ({ durationData }) => {
           axisLine={false}
           tickLine={false}
           tick={{ fontSize: 12, fontWeight: 500 }}
-          tickFormatter={convertDayNumberToDayLetter}
+          tickFormatter={dayNumberToLetter}
           stroke="rgba(255, 255, 255, 0.5)"
           tickMargin={35}
         />
         <YAxis hide="true" domain={[]} />
         <Tooltip
           wrapperStyle={{ outline: 'none' }}
-          content={<TooltipGenerator />}
+          content={<DurationToolTip />}
           cursor={false}
         />
         <Line
@@ -123,7 +89,7 @@ const DurationChart = ({ durationData }) => {
   );
 };
 
-DurationChart.propTypes = {
+SessionsDurationChart.propTypes = {
   durationData: PropTypes.arrayOf(
     PropTypes.shape({
       day: PropTypes.number.isRequired,
@@ -132,4 +98,4 @@ DurationChart.propTypes = {
   ).isRequired,
 };
 
-export default DurationChart;
+export default SessionsDurationChart;

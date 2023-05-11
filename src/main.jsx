c40845d1c98ecea94f26c7386edcf6648dd08361
Jeremy <jeremy.teurterie@gmail.com>
2023-05-11 18:08:56 +0700

    fix: edit main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './router';
+import Rooter from './router';
 
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <Rooter />
   </React.StrictMode>
 );
